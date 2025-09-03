67f908d3b3eb9eed53218464c96273adeeadf2b9
Vladyslav Savisko <savskovladik@gmail.com>
2025-09-03 20:21:24 +0300

    fix 2 files

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import pino from 'pino-http';
@@ -9,7 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
 
-const PORT = Number(getEnvVar('PORT', '3000'));
+const PORT = process.env.PORT || Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
     const app = express();
@@ -27,22 +26,18 @@ export const setupServer = () => {
 
     app.use(cookieParser());
 
-
     app.get('/', (req, res) => {
-        res.json({
-            message: 'Hello World!',
-        });
+        res.json({ message: 'Hello World!' });
     });
 
     app.use(router);
 
     app.use(notFoundHandler);
-
     app.use(errorHandler);
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-
     app.use('/uploads', express.static(UPLOAD_DIR));
+
+    app.listen(PORT, () => {
+        console.log(`✅ Server is running on port ${PORT}`);
+    });
 };
